d93cf760f1ac9fa2f28d788f6b40417bbf9cd7bb
brunocoelhorg <brunocoelhorg@yahoo.com.br>
2026-08-25 17:34:19 -0300

    25/08

---
+++ Logica-Programação/Java/Exercicios_lista02/Exercicio45.java
@@ -0,0 +1,32 @@
+/*
+Resultado de campeonato
+Leia os gols de dois times em uma partida e os pontos que cada time possuía antes do jogo.
+
+Regras de pontuação:
+
+vitória: 3 pontos;
+empate: 1 ponto para cada;
+derrota: 0 pontos.
+O programa deve:
+
+validar que os gols e pontos não são negativos;
+identificar vencedor ou empate;
+atualizar os pontos de cada time;
+informar qual time ficou com mais pontos após a partida;
+informar se os dois terminaram com a mesma pontuação.
+*/
+
+import java.util.Scanner;
+
+public class Exercicio45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Resultado de campeonato");
+        System.out.println("");
+        System.out.println("");
+
+        sc.close();
+    }
+    
+}
